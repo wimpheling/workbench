9d8c08014c1eb73215f2335a1682631bb0acf7b6
mezig <wimpheling@gmail.com>
2026-03-24 23:33:13 +0000

    fix

--- src/lib/Renderer3D.tsx
+++ src/lib/Renderer3D.tsx
@@ -8,7 +8,8 @@ export function Renderer3D({ object3D }: { object3D: MyObject3D }) {
       <div style={{ top: 0, left: 0, 'z-index': 1004, display: 'flex' }}>
         <ul style={{ 'list-style': 'none', display: 'flex' }}>
           {projects.map((project) => (
-            <li style={{ 'margin-right': '10px' }} key={project}>
+            // biome-ignore lint/correctness/useJsxKeyInIterable: <explanation>
+            <li style={{ 'margin-right': '10px' }}>
               <a style={{ 'text-decoration': 'none' }} href={`/projects/${project}.html`}>
                 {project}
               </a>
